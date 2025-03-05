8f9c2413ab09aad78092ffd57061118eec883f62
rosa <rosahadi19@gmail.com>
2025-03-05 21:54:53 +0300

    chore: initialize Next.js project with dependencies and configurations

---
+++ src/app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import {
+  APP_DESCRIPTION,
+  APP_NAME,
+  SERVER_URL,
+} from "@/lib/constants";
+import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/sonner"
+ 
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    template: `%s | Urban Edge`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
