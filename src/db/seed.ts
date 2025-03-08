1d9ca98882d1258ef5471e52836b3a58306cc1eb
rosa <rosahadi19@gmail.com>
2025-03-08 21:24:02 +0300

    feat(auth, seed): Implement bcrypt password hashing and update database seeding

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,13 +1,28 @@
 import { PrismaClient } from "@prisma/client";
 import sampleData from "./sample-data";
+import { hashPassword } from "@/lib/encrypt";
 
 async function main() {
   const prisma = new PrismaClient();
   await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
 
   await prisma.product.createMany({
     data: sampleData.products,
   });
+  const users = [];
+  for (let i = 0; i < sampleData.users.length; i++) {
+    users.push({
+      ...sampleData.users[i],
+      password: await hashPassword(
+        sampleData.users[i].password
+      ),
+    });
+  }
+  await prisma.user.createMany({ data: users });
 
   console.log("Database seeded successfully!");
 }
